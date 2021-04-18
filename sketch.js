3a2c1660d797f95a8f6743885757f01cb15509b2
adharsha11 <74136819+adharsha11@users.noreply.github.com>
2021-04-18 12:05:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Mouse = Matter.Mouse;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var backgroundImg
+var play, playImg
+var render 
+var mConstraint;
+var scrollImg, scroll
+
+
+var gameState = "0"
+function preload() {
+//loading the images
+    backgroundImg = loadImage("images/background4.jpg");
+    playImg = loadImage("images/play.png");
+    scrollImg = loadImage("images/scroll-removebg-preview.png")
+}
+
+
+function setup(){
+    var canvas = createCanvas(displayWidth, displayHeight);
+    engine = Engine.create();
+    world = engine.world;
+
+    play= createSprite(displayWidth/2-150 ,displayHeight/2 -70 , 320, 250)
+    play.addAnimation("click",playImg)
+    play.scale = 0.5;
+
+    
+}
+
+function draw(){
+    background(backgroundImg);
+    Engine.update(engine);
+//Creating the play button
+
+    
+   if (mousePressedOver(play)) {
+    play.visible= false;
+    scroll =createSprite(displayWidth/2-150 ,displayHeight/2 -70 , 320, 250)
+    scroll.addAnimation("click",scrollImg)
+    scroll.scale =0.5;
+  }
+   drawSprites();
+}
+
+
+
+
